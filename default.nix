75cdf19f1205597e39c035c4a76c0112428526ce
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2023-02-14 09:50:00 +0530

    Upgrade streamly version

--- default.nix
+++ default.nix
@@ -48,7 +48,7 @@ let haskellPackages =
                         #  } {})
                         (let src = fetchGit {
                              url = "git@github.com:composewell/streamly.git";
-                             rev = "3c3ec6de4831a8667c415393e1dbc351ba508dbe";
+                             rev = "96d222e45cf3aee9b6847c0d14fde967a760fee8";
                         }; in super.callCabal2nix "streamly" src {})
                         (old:
                           { librarySystemDepends =
@@ -64,7 +64,7 @@ let haskellPackages =
                       nixpkgs.haskell.lib.overrideCabal
                         (let src = fetchGit {
                             url = "git@github.com:composewell/streamly.git";
-                            rev = "3c3ec6de4831a8667c415393e1dbc351ba508dbe";
+                            rev = "96d222e45cf3aee9b6847c0d14fde967a760fee8";
                         }; in super.callCabal2nix "streamly-core" "${src}/core" {})
                         (old:
                           { librarySystemDepends =
